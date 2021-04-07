952b4a746dedca15189b156cdb197b20cdf3f3f9
tfettrow <tfettrow@gmail.com>
2021-04-07 18:39:26 -0400

    just an adding an example

--- fmri_processing/export_fmri_rois_to_redcap.sh
+++ fmri_processing/export_fmri_rois_to_redcap.sh
@@ -11,8 +11,7 @@
 
 # this script requires arguments 
 
-# example >> export_fmri_rois_to_redcap.sh '1002,1004,1007,1009,1010,1011,1013,1020,1022,1024,1027,2002,2007,2008,2012,2013,2015,2017,2018,2020,2021,2022,2023,2025,2026,2033,2034,2037,2042,2052' 05_MotorImagery ROI_settings_MiMRedcap.txt
-# example >> export_fmri_rois_to_redcap.sh '2052' 05_MotorImagery ROI_settings_MiMRedcap.txt
+# example >> export_fmri_rois_to_redcap.sh '1002,1004,1007,1009,1010,1011,1013,1020,1022,1024,1026,1027,2002,2007,2008,2012,2013,2015,2017,2018,2020,2021,2022,2023,2025,2026,2027,2033,2034,2037,2042,2052' 05_MotorImagery ROI_settings_MiMRedcap_NewAcc.txt fmri_roi_betas.csv
 
 ##################################################
 
