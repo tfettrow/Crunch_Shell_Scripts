af989a9bff976093430aefa4893d284722583943
tfettrow <tfettrow@gmail.com>
2021-07-19 15:41:39 -0400

    run_tbss now includes inversion warps.. gotta be a better way

--- dwi_processing/run_tbss.sh
+++ dwi_processing/run_tbss.sh
@@ -9,20 +9,23 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-ml fsl
+ml fsl/6.0.3
 Study_dir=/blue/rachaelseidler/share/FromExternal/Research_Projects_UF/CRUNCH/MiM_Data/
 cd "${Study_dir}/TBSS_results"
 
+###### Running TBSS preprocessing Steps ###########
 tbss_1_preproc *.nii
-
 tbss_2_reg -T
-
-#if you wish to use the FMRIB58_FA mean FA image and its derived skeleton, instead of the mean of your subjects in the study, use the -T option:
+# if you wish to use the FMRIB58_FA mean FA image and its derived skeleton, instead of the mean of your subjects in the study, use the -T option:
 tbss_3_postreg -T
-# tbss_3_postreg -S
+tbss_4_prestats 0.2   # do we have to run this here?? or is this a part of "stats"
+tbss_non_FA FW
+##################################################################
 
-
-# cd stats
-# fslview all_FA -b 0,0.8 mean_FA_skeleton -b 0.2,0.8 -l Green
-
-# tbss_4_prestats 0.2
+###### Warping MiM MNI ROIs in to Subject Space ###########
+cd "${Study_dir}/TBSS_results/FA"
+for this_warp in *tensorfit_eddycorrected_driftcorrected_DWI_FA_FA_to_target_warp.nii.gz; do
+	echo $this_warp
+	this_warp_file_name=$(echo $this_warp | cut -d. -f 1)
+	invwarp --ref=target --warp=${this_warp_file_name} --out=${this_warp_file_name}_inv
+done
