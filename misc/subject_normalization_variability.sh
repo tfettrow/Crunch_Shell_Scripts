006b4c52aba4370a2f77895854b2d6a0be789f49
tfettrow <tfettrow@gmail.com>
2021-12-29 10:31:07 -0500

    Update subject_normalization_variability.sh

--- misc/subject_normalization_variability.sh
+++ misc/subject_normalization_variability.sh
@@ -74,16 +74,23 @@ export MATLABPATH=${Code_dir}/Matlab_Scripts/helper
 Study_dir=/blue/rachaelseidler/share/FromExternal/Research_Projects_UF/CRUNCH/MiM_Data
 
 # subjects='2007,2012,2008,2021,2015'
-subjects='1002,1004,1010,1011,1013'
+# subjects='1002,1004,1010,1011,1013'
+# subjects='1002,1004,1009,1010,1013,1018,1019,1020,1022,1026,1027'
+# subjects='2002,2007,2008,2012,2013,2015,2017,2018,2020,2021,2023,2025,2026,2033,2034,2037,2038,2039,2042,2052,2059,2062,2027'
+subjects='3004,3006,3007,3008,3010,3021,3023,3024,3025,3026,3027,3028,3029,3030,3034,3036,3039,3040,3042,3043,3046,3047,3051,3053,3056,3058,3059,3063,3066,3068'
 # subjects='wu120_001,wu120_002,wu120_003,wu120_004,wu120_005'
 
-output_filename=mim_subject_normalized_intensity_variability.nii
+output_filename=mim_asl_subject_normalized_intensity_variability.nii
 # file_to_compare='warpedToSUITdartelNoBrainstem_coregToSUIT_CBmasked_coregToT1_unwarpedRealigned_slicetimed_fMRI_Run1.nii'
 # file_to_compare='warpedToMNI_unwarpedRealigned_slicetimed_RestingState.nii'
-file_to_compare='warpedToMNI_unwarpedRealigned_slicetimed_fMRI_Run1.nii'
-image_folder='05_MotorImagery'
+# file_to_compare='warpedToMNI_unwarpedRealigned_slicetimed_fMRI_Run1.nii'
+
+file_to_compare='warpedToMNI_perfusion_calib.nii'
+# image_folder='05_MotorImagery'
 # image_folder='06_Nback'
 # image_folder='04_rsfMRI'
+# image_folder='07_ASL/BasilCMD_calib_anat_scalib_pvcorr'
+image_folder='07_ASL/BasilCMD_calib_anat_FM_scalib_pvcorr'
 
 while IFS=',' read -ra subject_list; do
    for this_subject in "${subject_list[@]}"; do
